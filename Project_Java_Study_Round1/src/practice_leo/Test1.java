3f1536d574f78966ccfd969421e43d973621233a
leo <leo@blinkl.kr>
2013-03-08 01:10:54 +0900

    leo 1st commit

---
+++ Project_Java_Study_Round1/src/practice_leo/Test1.java
@@ -0,0 +1,15 @@
+package practice_leo;
+
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test");
+
+	}
+
+}
---
+++ Project_Java_Study_Round1/src/practice_leo/helloworld.java
@@ -0,0 +1,18 @@
+package practice_leo;
+
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello world");
+		System.out.println("you are new, aren't you?");
+	}
+
+}
