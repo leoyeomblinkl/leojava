e2e494d1bcd227b4c99818d1aeedecec6ccae3d5
leo <leo@blinkl.kr>
2013-03-16 01:58:26 +0900

    Ex04 studying

---
+++ Project_Java_Study_Round1/src/Ex04/Ex0413.java
@@ -0,0 +1,31 @@
+package Ex04;
+
+public class Ex0413 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String value ="12o45";
+		char ch=' ';
+		boolean isNumber = true;
+		
+		// charat(int i)
+		
+		for (int i = 1; i <= value.length(); i++) {
+			if(('0'<=value.charAt(i))&&(value.charAt(i)<='9')){
+				isNumber=true;
+			} else {
+				isNumber=false;
+				break;
+			}
+		}
+		if (isNumber) {
+			System.out.println(value+" is number");
+		} else {
+			System.out.println(value+" is NOT number");
+		}
+	}
+
+}
---
+++ Project_Java_Study_Round1/src/Ex04/Ex0415.java
@@ -0,0 +1,23 @@
+package Ex04;
+
+public class Ex0415 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number= 12321;
+		int tmp = number;
+		
+		int result=0;
+		
+		while(tmp !=0){
+			
+		}
+			
+
+	}
+
+}
