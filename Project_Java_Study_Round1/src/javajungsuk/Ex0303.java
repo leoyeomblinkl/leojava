99354049360f52ae50094fd04dd89e7cff442dd8
leo <leo@blinkl.kr>
2013-03-15 19:03:00 +0900

    ex03

---
+++ Project_Java_Study_Round1/src/javajungsuk/Ex0303.java
@@ -0,0 +1,17 @@
+package javajungsuk;
+
+public class Ex0303 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=10;
+		System.out.println((num>=0)? ((num==0)? "0" : "양수") : "음수"
+		);
+		
+	}
+
+}
