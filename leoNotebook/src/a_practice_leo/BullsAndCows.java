37cd9504554414187b8c1f4f6e79e66681a74609
leo <leo@blinkl.kr>
2013-03-19 01:56:35 +0900

    update bullsAndcows

--- leoNotebook/src/a_practice_leo/BullsAndCows.java
+++ leoNotebook/src/a_practice_leo/BullsAndCows.java
@@ -1,6 +1,6 @@
 package a_practice_leo;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class BullsAndCows {
 
@@ -10,13 +10,13 @@ public class BullsAndCows {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] number=new int[9];
+		int[] number=new int[9]; // variables declaration
 		int bullCount=0;
 		int cowCount=0;
-		String startMessage="";
+//		String startMessage="";  
 		int count=0;
 		
-		for (int i = 0; i < 9; i++) { // put 1~9 numbers into number array
+		for (int i = 0; i < 9; i++) { // put numbers into number array
 			number[i]=i+1;
 		}
 		
@@ -29,64 +29,87 @@ public class BullsAndCows {
 			number[0]=temp;
 		}
 		
-		int[] answer=new int[4]; // answer 
-		System.arraycopy(number, 0, answer, 0, 4); // pick first 4 numbers from number array to answer array
 		
+		int[] answer=new int[4]; // answer array declaration
+		System.arraycopy(number, 0, answer, 0, 4); // copy first four number array to answer array
 		
-//		System.out.println("The answer is "+ "' "+answer[0]+" "+answer[1]+" "+answer[2]+" "+answer[3]+" '"); // for checking
+		Scanner scan = new Scanner(System.in);
+		String inputNumber;
+		
+//		System.out.println("Á¤´äÀº  "+ "' "+answer[0]+" "+answer[1]+" "+answer[2]+" "+answer[3]+" '"); // *cheat key* for showing answer
+		System.out.print("Put four non-repetive numbers which are correct guess : ");
 		
 		Loop1 : do{
 			
 			++count;
 			
-			startMessage = JOptionPane.showInputDialog("put 4 numbers of your guess!!");
+			String inputNumberTemp="";
+			inputNumberTemp=scan.nextLine();
 			
-			int input = 0;
-			input=Integer.parseInt(startMessage);
 			
-			System.out.println("==========Round"+count+"========");
+			int input=0;
+			inputNumber=inputNumberTemp.replaceAll(" ", "");
+//			System.out.println(inputNumber); // for space bar check
+			
+			for (int i = 0; i < inputNumber.length(); i++) { // casting string to integer
+				if ('1'<=inputNumber.charAt(i)&&inputNumber.charAt(i)<='9') {
+					int temp=inputNumber.charAt(i)-'0';
+					for (int j = 0; j < inputNumber.length()-i-1; j++) {
+						temp*=10;	
+					}
+					input+=temp;
+					
+//					System.out.println(input); // for checking
+					
+				} else {
+					System.out.println("Out of range! Please choose numbers from 1 to 9 :)");
+					continue Loop1;
+				}
+			}
+			
+			
+			
+			System.out.println("==========Round: "+count+"========");
 
 		
 		
 			if(999>=input||input>=10000){
-				System.out.println("You must put 4 numbers. Please try again");
+				System.out.println("Please put 4 numbers. Let's try again.");
 				continue Loop1;
 			}
 		
 		int[] guess=new int[4];
-		for (int i = guess.length; i > 0; i--) { // put 4 each numbers into array guess
+		for (int i = guess.length; i > 0; i--) { // cast input to array
 			guess[i-1]=input%10;
 			input/=10;
 			
 		}
 		
-//		if (input!=0) { // check number count. must me 4numbers.
-//			System.out.println("You put more than 4 numbers. Please put 4 numbers");
-//			continue Loop1;
-//		}
+
 		
-		for (int i = 0; i < guess.length-1; i++) { // check repetition of numbers
+		for (int i = 0; i < guess.length-1; i++) { // check for repetitive numbers
 			for (int j = 0; j < guess.length-1-i; j++) {
 							
 				if(guess[i]==guess[i+1] ){
-					System.out.println(" You must put 4 different numbers to each other");
+					System.out.println("Please put non-repetitive numbers. Let's try again.");
 					continue Loop1;
 			}
 			}
 		}
 		
-//		System.out.println(guess[0]+" "+guess[1]+" "+guess[2]+" "+guess[3]); //check for guess
+		System.out.println(guess[0]+" "+guess[1]+" "+guess[2]+" "+guess[3]); // Showing guess
+		
 		bullCount=0;
 		cowCount=0;
 		
-		for (int i = 0; i < guess.length; i++) { // get bull count
+		for (int i = 0; i < guess.length; i++) { // bull count
 			if (guess[i]==answer[i]) {
 				++bullCount;
 				
 			}
 		}
 		
-		for (int i = 0; i < guess.length; i++) { // get cow count
+		for (int i = 0; i < guess.length; i++) { // cow count
 			for (int j = 0; j < guess.length; j++) {
 				if (guess[i]==answer[j] && guess[i]!=answer[i]) {
 					++cowCount;
@@ -94,13 +117,13 @@ public class BullsAndCows {
 			}
 		}
 		
-				System.out.println("bullcount : "+bullCount);
-		System.out.println("cowcount : "+cowCount);
+				System.out.println("Bull: "+bullCount);
+		System.out.println("cow: "+cowCount);
 
 		if(bullCount==4) {
 			System.out.println("=-=-================-=-=");
-			System.out.println("The answer is "+ "' "+answer[0]+" "+answer[1]+" "+answer[2]+" "+answer[3]+" '");
-			System.out.println("you got the right answer!! ( "+count+" round )");
+			System.out.println("Wow!! Bulls!!! "+ "' "+answer[0]+" "+answer[1]+" "+answer[2]+" "+answer[3]+" '!");
+			System.out.println("Congraturation! you got me on "+count+" rounds");
 			break;
 		}
 		
